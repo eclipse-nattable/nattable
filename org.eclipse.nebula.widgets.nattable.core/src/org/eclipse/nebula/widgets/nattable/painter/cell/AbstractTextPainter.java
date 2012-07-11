eclipse-nattable/nattable

commit 7b7c3a1a6dba5d883968fcb3990e5d3ed6567f29

bug 383877: fixed divide by zero and corrected divisor

org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/painter/cell/AbstractTextPainter.java
@@ -347,8 +347,8 @@ public abstract class AbstractTextPainter extends BackgroundPainter {
 					int newTextExtent = getLengthFromCache(gc, trialLabelText);
 					
 					while (newTextExtent > availableLength + 1 && newStringLength > 0) {
-						int avgWidthPerChar = newTextExtent / line.length();
-						numExtraChars = 1 + (newTextExtent - availableLength) / avgWidthPerChar;
+						double avgWidthPerChar = (double) newTextExtent / trialLabelText.length();
+						numExtraChars = 1 + (int) ((newTextExtent - availableLength) / avgWidthPerChar);
 						
 						newStringLength = nextTrialString.length() - numExtraChars;
 						if (newStringLength>0){

commit 7b6aa2e7a40bf3925aa3376539e0c16dae21e40c

bug 383877: fixed shortening text in text painter (regression introduced in 7b7c3a1a6dba5d883968fcb3990e5d3ed6567f29)

org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/painter/cell/AbstractTextPainter.java
@@ -339,25 +339,23 @@ public abstract class AbstractTextPainter extends BackgroundPainter {
 				//this way every line will get ... if it doesn't fit
 				int lineLength = getLengthFromCache(gc, line);
 				if (lineLength > availableLength) {
-					String nextTrialString = line;
 					int numExtraChars = 0;
-					int newStringLength = nextTrialString.length();
-					
-					String trialLabelText = nextTrialString + DOT;
+
+					int newStringLength = line.length();
+					String trialLabelText = line + DOT;
 					int newTextExtent = getLengthFromCache(gc, trialLabelText);
-					
+
 					while (newTextExtent > availableLength + 1 && newStringLength > 0) {
 						double avgWidthPerChar = (double) newTextExtent / trialLabelText.length();
-						numExtraChars = 1 + (int) ((newTextExtent - availableLength) / avgWidthPerChar);
-						
-						newStringLength = nextTrialString.length() - numExtraChars;
-						if (newStringLength>0){
-							nextTrialString = nextTrialString.substring(0, newStringLength);
-							trialLabelText = nextTrialString + DOT;
+						numExtraChars += 1 + (int) ((newTextExtent - availableLength) / avgWidthPerChar);
+
+						newStringLength = line.length() - numExtraChars;
+						if (newStringLength > 0) {
+							trialLabelText = line.substring(0, newStringLength) + DOT;
 							newTextExtent = getLengthFromCache(gc, trialLabelText);
 						}
 					}
-					
+
 					if (numExtraChars > line.length()) {
 						numExtraChars = line.length();
 					}