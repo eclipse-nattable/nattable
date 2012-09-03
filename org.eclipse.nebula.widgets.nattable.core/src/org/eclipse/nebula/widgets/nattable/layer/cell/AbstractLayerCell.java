7ee90981eaff1e7393443147fa12a235001dc160
Dirk Fauth <dirk.fauth@googlemail.com>
2012-09-03 10:54:47 +0200

    Bug 387813 - added missing EPL license headers

--- org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/layer/cell/AbstractLayerCell.java
+++ org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/layer/cell/AbstractLayerCell.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Edwin Park and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Edwin Park - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.nebula.widgets.nattable.layer.cell;
 
 import org.eclipse.nebula.widgets.nattable.layer.LabelStack;
