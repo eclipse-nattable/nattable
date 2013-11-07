c8e24a59b9728dbb10eb37604a54407753784fac
Dirk Fauth <dirk.fauth@googlemail.com>
2013-11-07 14:01:52 +0100

    Bug 421244 - Added API to get all child objects of a tree node instead of only the visible child indexes

--- org.eclipse.nebula.widgets.nattable.extension.glazedlists/src/org/eclipse/nebula/widgets/nattable/extension/glazedlists/tree/GlazedListTreeRowModel.java
+++ org.eclipse.nebula.widgets.nattable.extension.glazedlists/src/org/eclipse/nebula/widgets/nattable/extension/glazedlists/tree/GlazedListTreeRowModel.java
@@ -11,126 +11,38 @@
 package org.eclipse.nebula.widgets.nattable.extension.glazedlists.tree;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 
-import org.eclipse.nebula.widgets.nattable.tree.ITreeRowModel;
-import org.eclipse.nebula.widgets.nattable.tree.ITreeRowModelListener;
+import org.eclipse.nebula.widgets.nattable.tree.AbstractTreeRowModel;
 
 
-public class GlazedListTreeRowModel<T> implements ITreeRowModel<T>{
-
-	private final Collection<ITreeRowModelListener> listeners = new HashSet<ITreeRowModelListener>();
-
-	private final GlazedListTreeData<T> treeData;
+public class GlazedListTreeRowModel<T> extends AbstractTreeRowModel<T>{
 
 	public GlazedListTreeRowModel(GlazedListTreeData<T> treeData) {
-		this.treeData = treeData;
-	}
-
-	public void registerRowGroupModelListener(ITreeRowModelListener listener) {
-		this.listeners.add(listener);
-	}
-
-	public void notifyListeners() {
-		for (ITreeRowModelListener listener : this.listeners) {
-			listener.treeRowModelChanged();
-		}
-	}
-
-	@Override
-	public int depth(int index) {
-		return this.treeData
-				.getDepthOfData(this.treeData.getDataAtIndex(index));
-	}
-
-	@Override
-	public boolean isLeaf(int index) {
-		return !hasChildren(index);
-	}
-
-	@Override
-	public String getObjectAtIndexAndDepth(int index, int depth) {
-		return this.treeData.formatDataForDepth(depth, index);
-	}
-
-	@Override
-	public boolean hasChildren(int index) {
-		return this.treeData.hasChildren(index);
+		super(treeData);
 	}
 
 	@Override
 	public boolean isCollapsed(int index) {
-		return !this.treeData.isExpanded(index);
-	}
-
-	public void clear() {
-		
-	}
-
-	/**
-	 * @return TRUE if the row group this index is collapseable
-	 */
-	@Override
-	public boolean isCollapseable(int index) {
-		return hasChildren(index);
+		return !this.getTreeData().isExpanded(index);
 	}
 
 	@Override
 	public List<Integer> collapse(int index) {
-		this.treeData.collapse(index);
+		this.getTreeData().collapse(index);
 		notifyListeners();
 		return new ArrayList<Integer>();
 	}
 
 	@Override
 	public List<Integer> expand(int index) {
-		this.treeData.expand(index);
+		this.getTreeData().expand(index);
 		notifyListeners();
 		return new ArrayList<Integer>();
 	}
 
 	@Override
-	public List<Integer> getChildIndexes(int parentIndex) {
-		List<Integer> result = new ArrayList<Integer>();
-		List<T> children = this.treeData.getChildren(parentIndex);
-		for (T child : children) {
-			int index = this.treeData.indexOf(child);
-			//if the index is -1 the element is not found
-			//this means it is not visible and therefore can not be handled
-			if (index >= 0) {
-				result.add(index);
-				result.addAll(getChildIndexes(index));
-			}
-		}
-		return result;
+	protected GlazedListTreeData<T> getTreeData() {
+		return (GlazedListTreeData<T>) super.getTreeData();
 	}
-	
-	@Override
-	public List<Integer> getDirectChildIndexes(int parentIndex) {
-		List<Integer> result = new ArrayList<Integer>();
-		List<T> children = this.treeData.getChildren(parentIndex);
-		for (T child : children) {
-			int index = this.treeData.indexOf(child);
-			//if the index is -1 the element is not found
-			//this means it is not visible and therefore can not be handled
-			if (index >= 0) {
-				result.add(index);
-			}
-		}
-		return result;
-	}
-
-	@Override
-	public List<Integer> getRootIndexes() {
-		List<Integer> result = new ArrayList<Integer>();
-		List<T> roots = this.treeData.getRoots();
-		for (T root : roots) {
-			int index = this.treeData.indexOf(root);
-			result.add(index);
-		}
-		return result;
-	}
-
 }
