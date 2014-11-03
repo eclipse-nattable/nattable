afbe9e49a5e42615f49916ebc805c6e43f9bee11
nzhangihl <nujiah001@126.com>
2014-11-03 15:03:12 +0800

    Bug 448934 - Public the SizeConfig from DataLayer
    Change-Id: Ie704dc205ab1cf02b8a4c1866f8fafcc6d4dd0f7
    Signed-off-by: nzhangihl <nujiah001@126.com>

--- org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/layer/DataLayer.java
+++ org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/layer/DataLayer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2013 Original authors and others.
+ * Copyright (c) 2012, 2013, 2014 Original authors and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Original authors and others - initial API and implementation
+ *     neal zhang <nujiah001@126.com> - Bug 448934
  ******************************************************************************/
 
 package org.eclipse.nebula.widgets.nattable.layer;
@@ -49,8 +50,8 @@ public class DataLayer extends AbstractLayer implements IUniqueIndexLayer {
 
     protected IDataProvider dataProvider;
 
-    private final SizeConfig columnWidthConfig;
-    private final SizeConfig rowHeightConfig;
+    protected SizeConfig columnWidthConfig;
+    protected SizeConfig rowHeightConfig;
 
     public DataLayer(IDataProvider dataProvider) {
         this(dataProvider, DEFAULT_COLUMN_WIDTH, DEFAULT_ROW_HEIGHT);
