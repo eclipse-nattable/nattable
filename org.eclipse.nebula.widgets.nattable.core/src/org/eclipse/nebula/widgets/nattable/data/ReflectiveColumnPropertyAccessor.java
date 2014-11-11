1cf376dcfeb934d038a84a086128ed5caa30a317
Dirk Fauth <dirk.fauth@googlemail.com>
2014-11-11 12:37:10 +0100

    Bug 450927 - added synchronized handling for building up the property descriptor map

--- org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/data/ReflectiveColumnPropertyAccessor.java
+++ org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/data/ReflectiveColumnPropertyAccessor.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2013 Original authors and others.
+ * Copyright (c) 2012, 2013, 2014 Original authors and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Original authors and others - initial API and implementation
+ *     Dirk Fauth <dirk.fauth@googlemail.com> - Bug 450927
  ******************************************************************************/
 package org.eclipse.nebula.widgets.nattable.data;
 
@@ -30,11 +31,9 @@ import org.apache.commons.logging.LogFactory;
  * @param <R>
  *            type of the row object/bean
  */
-public class ReflectiveColumnPropertyAccessor<R> implements
-        IColumnPropertyAccessor<R> {
+public class ReflectiveColumnPropertyAccessor<R> implements IColumnPropertyAccessor<R> {
 
-    private static final Log log = LogFactory
-            .getLog(ReflectiveColumnPropertyAccessor.class);
+    private static final Log log = LogFactory.getLog(ReflectiveColumnPropertyAccessor.class);
 
     private final List<String> propertyNames;
 
@@ -48,14 +47,15 @@ public class ReflectiveColumnPropertyAccessor<R> implements
         this.propertyNames = Arrays.asList(propertyNames);
     }
 
+    @Override
     public int getColumnCount() {
-        return propertyNames.size();
+        return this.propertyNames.size();
     }
 
+    @Override
     public Object getDataValue(R rowObj, int columnIndex) {
         try {
-            PropertyDescriptor propertyDesc = getPropertyDescriptor(rowObj,
-                    columnIndex);
+            PropertyDescriptor propertyDesc = getPropertyDescriptor(rowObj, columnIndex);
             Method readMethod = propertyDesc.getReadMethod();
             return readMethod.invoke(rowObj);
         } catch (Exception e) {
@@ -64,10 +64,10 @@ public class ReflectiveColumnPropertyAccessor<R> implements
         }
     }
 
+    @Override
     public void setDataValue(R rowObj, int columnIndex, Object newValue) {
         try {
-            PropertyDescriptor propertyDesc = getPropertyDescriptor(rowObj,
-                    columnIndex);
+            PropertyDescriptor propertyDesc = getPropertyDescriptor(rowObj, columnIndex);
             Method writeMethod = propertyDesc.getWriteMethod();
             if (writeMethod == null) {
                 throw new RuntimeException(
@@ -75,36 +75,37 @@ public class ReflectiveColumnPropertyAccessor<R> implements
             }
             writeMethod.invoke(rowObj, newValue);
         } catch (IllegalArgumentException ex) {
-            log.error(
-                    "Data type being set does not match the data type of the setter method in the backing bean", ex); //$NON-NLS-1$
+            log.error("Data type being set does not match the data type of the setter method in the backing bean", ex); //$NON-NLS-1$
         } catch (Exception e) {
             log.error(e);
             throw new RuntimeException("Error while setting data value"); //$NON-NLS-1$
         }
     };
 
+    @Override
     public String getColumnProperty(int columnIndex) {
-        return propertyNames.get(columnIndex);
+        return this.propertyNames.get(columnIndex);
     }
 
+    @Override
     public int getColumnIndex(String propertyName) {
-        return propertyNames.indexOf(propertyName);
+        return this.propertyNames.indexOf(propertyName);
     }
 
-    private PropertyDescriptor getPropertyDescriptor(R rowObj, int columnIndex)
-            throws IntrospectionException {
-        if (propertyDescriptorMap == null) {
-            propertyDescriptorMap = new HashMap<String, PropertyDescriptor>();
-            PropertyDescriptor[] propertyDescriptors = Introspector
-                    .getBeanInfo(rowObj.getClass()).getPropertyDescriptors();
-            for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
-                propertyDescriptorMap.put(propertyDescriptor.getName(),
-                        propertyDescriptor);
+    private PropertyDescriptor getPropertyDescriptor(R rowObj, int columnIndex) throws IntrospectionException {
+        synchronized (rowObj) {
+            if (this.propertyDescriptorMap == null) {
+                this.propertyDescriptorMap = new HashMap<String, PropertyDescriptor>();
+                PropertyDescriptor[] propertyDescriptors =
+                        Introspector.getBeanInfo(rowObj.getClass()).getPropertyDescriptors();
+                for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
+                    this.propertyDescriptorMap.put(propertyDescriptor.getName(), propertyDescriptor);
+                }
             }
         }
 
-        final String propertyName = propertyNames.get(columnIndex);
-        return propertyDescriptorMap.get(propertyName);
+        final String propertyName = this.propertyNames.get(columnIndex);
+        return this.propertyDescriptorMap.get(propertyName);
     }
 
 }
