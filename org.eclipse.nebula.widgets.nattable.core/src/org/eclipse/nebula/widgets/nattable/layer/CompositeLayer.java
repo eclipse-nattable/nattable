90f92f87c0e8ab65c8ab4340d61ee2c3b6088213
Dirk Fauth <dirk.fauth@googlemail.com>
2014-11-24 21:08:58 +0100

    Bug 451152 - corrected getChildClientArea(int, int) for remaining space calculation

--- org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/layer/CompositeLayer.java
+++ org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/layer/CompositeLayer.java
@@ -679,6 +679,38 @@ public class CompositeLayer extends AbstractLayer {
 
         final Rectangle compositeClientArea = getClientAreaProvider().getClientArea();
 
+        // check if there are further sections to the bottom and reduce the
+        // height accordingly necessary in case the current child layer is
+        // a viewport layer that takes the whole space
+        if (this.childLayerLayout[layoutX].length > layoutY + 1) {
+            for (int bottomY = (layoutY + 1); bottomY < this.childLayerLayout[layoutX].length; bottomY++) {
+                ILayer bottomChildLayer = this.childLayerLayout[layoutX][bottomY];
+                int bottomHeight = bottomChildLayer.getPreferredHeight();
+                // if the layer to the bottom is bigger than the client area
+                // we assume it is the layer that takes the remaining space
+                // e.g. ViewportLayer
+                if (bottomHeight < compositeClientArea.height) {
+                    compositeClientArea.height -= bottomHeight;
+                }
+            }
+        }
+
+        // check if there are further sections to the right and reduce the
+        // width accordingly necessary in case the current child layer is
+        // a viewport layer that takes the whole space
+        if (this.childLayerLayout.length > layoutX + 1) {
+            for (int rightX = (layoutX + 1); rightX < this.childLayerLayout.length; rightX++) {
+                ILayer rightChildLayer = this.childLayerLayout[rightX][layoutY];
+                int rightWidth = rightChildLayer.getPreferredWidth();
+                // if the layer to the right is bigger than the client area
+                // we assume it is the layer that takes the remaining space
+                // e.g. ViewportLayer
+                if (rightWidth < compositeClientArea.width) {
+                    compositeClientArea.width -= rightWidth;
+                }
+            }
+        }
+
         final Rectangle childClientArea = new Rectangle(
                 compositeClientArea.x + getWidthOffset(layoutX),
                 compositeClientArea.y + getHeightOffset(layoutY),
@@ -686,32 +718,6 @@ public class CompositeLayer extends AbstractLayer {
                 childLayer.getPreferredHeight());
 
         final Rectangle intersection = compositeClientArea.intersection(childClientArea);
-
-        // handle further regions to bottom
-        if ((intersection.y + intersection.height) == compositeClientArea.height) {
-            // check if there are further sections to the bottom and reduce the
-            // height accordingly necessary in case the current child layer is
-            // a viewport layer that takes the whole space
-            if (this.childLayerLayout[layoutX].length > layoutY + 1) {
-                for (int bottomY = (layoutY + 1); bottomY < this.childLayerLayout[layoutX].length; bottomY++) {
-                    ILayer bottomChildLayer = this.childLayerLayout[layoutX][bottomY];
-                    intersection.height -= bottomChildLayer.getPreferredHeight();
-                }
-            }
-        }
-
-        // handle further regions to the right
-        if ((intersection.x + intersection.width) == compositeClientArea.width) {
-            // check if there are further sections to the right and reduce the
-            // width accordingly necessary in case the current child layer is
-            // a viewport layer that takes the whole space
-            if (this.childLayerLayout.length > layoutX + 1) {
-                for (int rightX = (layoutX + 1); rightX < this.childLayerLayout.length; rightX++) {
-                    ILayer bottomChildLayer = this.childLayerLayout[rightX][layoutY];
-                    intersection.width -= bottomChildLayer.getPreferredWidth();
-                }
-            }
-        }
 
         return intersection;
     }
