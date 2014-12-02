2b170fb56329945abf39eac914c553d15071cb21
Dirk Fauth <dirk.fauth@googlemail.com>
2014-12-02 14:36:41 +0100

    Bug 453882 - added DragModeEventHandler#focusLost(FocusEvent) to stop the drag mode if NatTable looses focus

--- org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/ui/action/DragModeEventHandler.java
+++ org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/ui/action/DragModeEventHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Original authors and others.
+ * Copyright (c) 2012, 2014 Original authors and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Original authors and others - initial API and implementation
+ *     Dirk Fauth <dirk.fauth@googlemail.com> - Bug 453882
  ******************************************************************************/
 package org.eclipse.nebula.widgets.nattable.ui.action;
 
@@ -15,6 +16,7 @@ import org.eclipse.nebula.widgets.nattable.ui.mode.AbstractModeEventHandler;
 import org.eclipse.nebula.widgets.nattable.ui.mode.Mode;
 import org.eclipse.nebula.widgets.nattable.ui.mode.ModeSupport;
 import org.eclipse.nebula.widgets.nattable.ui.mode.MouseModeEventHandler;
+import org.eclipse.swt.events.FocusEvent;
 import org.eclipse.swt.events.MouseEvent;
 
 public class DragModeEventHandler extends AbstractModeEventHandler {
@@ -24,9 +26,13 @@ public class DragModeEventHandler extends AbstractModeEventHandler {
     private final MouseModeEventHandler parentModeEventHandler;
     private final MouseEvent mouseDownEvent;
 
-    public DragModeEventHandler(ModeSupport modeSupport, NatTable natTable,
-            IDragMode dragMode, MouseModeEventHandler parentModeEventHandler,
+    public DragModeEventHandler(
+            ModeSupport modeSupport,
+            NatTable natTable,
+            IDragMode dragMode,
+            MouseModeEventHandler parentModeEventHandler,
             MouseEvent mouseDownEvent) {
+
         super(modeSupport, natTable);
 
         this.dragMode = dragMode;
@@ -52,4 +58,12 @@ public class DragModeEventHandler extends AbstractModeEventHandler {
         }
     }
 
+    @Override
+    public void focusLost(FocusEvent event) {
+        // Bug 453882
+        // NatTable lost the focus while a drag operation was active
+        // so we simple skip the drag operation by calling mouseUp using the
+        // initial mouseDown event
+        mouseUp(this.mouseDownEvent);
+    }
 }
