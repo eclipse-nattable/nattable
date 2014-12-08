914b603ded925eda68911c1c92c550937003e6d1
Roman Flückiger <roman.flueckiger@mac.com>
2014-12-08 15:58:13 +0100

    Bug 454440 - Call clearQueue before invoking another thread, to guarantee, that ever event added after/during the redraw process will be handled in the next conflater cycle and not lost.
    Change-Id: If90d80315494a3e54058791d530c563e5662761a
    Signed-off-by: Roman Flückiger <roman.flueckiger@mac.com>

--- org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/conflation/VisualChangeEventConflater.java
+++ org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/conflation/VisualChangeEventConflater.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Original authors and others.
+ * Copyright (c) 2012, 2014 Original authors and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Original authors and others - initial API and implementation
+ *     Roman Flueckiger <roman.flueckiger@mac.com> - Bug 454440
  ******************************************************************************/
 package org.eclipse.nebula.widgets.nattable.conflation;
 
@@ -15,9 +16,8 @@ import org.eclipse.nebula.widgets.nattable.layer.event.ILayerEvent;
 import org.eclipse.nebula.widgets.nattable.layer.event.IVisualChangeEvent;
 
 /**
- * Gathers all the VisualChangeEvents. When its run, it refreshes/repaints the
+ * Gathers all the VisualChangeEvents. When it's run, it refreshes/repaints the
  * table.
- *
  */
 public class VisualChangeEventConflater extends AbstractEventConflater {
 
@@ -41,14 +41,14 @@ public class VisualChangeEventConflater extends AbstractEventConflater {
             @Override
             public void run() {
                 if (VisualChangeEventConflater.this.queue.size() > 0) {
+                    clearQueue();
+
                     VisualChangeEventConflater.this.natTable.getDisplay().asyncExec(new Runnable() {
                         @Override
                         public void run() {
                             VisualChangeEventConflater.this.natTable.updateResize();
                         }
                     });
-
-                    clearQueue();
                 }
             }
         };
