bed2dc82eea2feff5f142d2cb033db767b75f470
Dirk Fauth <dirk.fauth@googlemail.com>
2014-12-15 14:29:21 +0100

    Bug 454909 - Added the ability to specify the sort direction via SortColumnCommand

--- org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/sort/command/SortColumnCommand.java
+++ org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/sort/command/SortColumnCommand.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Original authors and others.
+ * Copyright (c) 2012, 2014 Original authors and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,29 +7,125 @@
  *
  * Contributors:
  *     Original authors and others - initial API and implementation
+ *     Dirk Fauth <dirk.fauth@googlemail.com> - Bug 454909
  ******************************************************************************/
 package org.eclipse.nebula.widgets.nattable.sort.command;
 
 import org.eclipse.nebula.widgets.nattable.command.AbstractColumnCommand;
 import org.eclipse.nebula.widgets.nattable.layer.ILayer;
+import org.eclipse.nebula.widgets.nattable.sort.SortDirectionEnum;
+import org.eclipse.nebula.widgets.nattable.sort.SortHeaderLayer;
 
+/**
+ * Command to trigger sorting.
+ *
+ * @see SortHeaderLayer
+ * @see SortCommandHandler
+ */
 public class SortColumnCommand extends AbstractColumnCommand {
 
     private boolean accumulate;
+    private SortDirectionEnum sortDirection;
 
-    public SortColumnCommand(ILayer layer, int columnPosition,
-            boolean accumulate) {
-        super(layer, columnPosition);
-        this.accumulate = accumulate;
+    /**
+     * Create a {@link SortColumnCommand} that triggers sorting for replacing an
+     * existing sort state (no accumulate) without specifying a sort direction.
+     *
+     * @param layer
+     *            The layer to which the column position belongs.
+     * @param columnPosition
+     *            The position of the column for which the sorting should be
+     *            applied.
+     */
+    public SortColumnCommand(ILayer layer, int columnPosition) {
+        this(layer, columnPosition, false, null);
     }
 
+    /**
+     * Create a {@link SortColumnCommand} that triggers sorting without
+     * specifying a sort direction.
+     *
+     * @param layer
+     *            The layer to which the column position belongs.
+     * @param columnPosition
+     *            The position of the column for which the sorting should be
+     *            applied.
+     * @param accumulate
+     *            <code>true</code> if the sorting should be accumulated to an
+     *            already applied sorting, <code>false</code> if the sorting
+     *            should replace an existing sorting.
+     */
+    public SortColumnCommand(ILayer layer, int columnPosition, boolean accumulate) {
+        this(layer, columnPosition, accumulate, null);
+    }
+
+    /**
+     * Create a {@link SortColumnCommand} that triggers sorting for replacing an
+     * existing sort state (no accumulate).
+     *
+     * @param layer
+     *            The layer to which the column position belongs.
+     * @param columnPosition
+     *            The position of the column for which the sorting should be
+     *            applied.
+     */
+    public SortColumnCommand(ILayer layer, int columnPosition, SortDirectionEnum sortDirection) {
+        this(layer, columnPosition, false, sortDirection);
+    }
+
+    /**
+     *
+     * @param layer
+     *            The layer to which the column position belongs.
+     * @param columnPosition
+     *            The position of the column for which the sorting should be
+     *            applied.
+     * @param accumulate
+     *            <code>true</code> if the sorting should be accumulated to an
+     *            already applied sorting, <code>false</code> if the sorting
+     *            should replace an existing sorting.
+     * @param sortDirection
+     *            The sort direction that should be used for sorting. Can be
+     *            <code>null</code> which means that the sort direction will be
+     *            used that is calculated by the ISortModel for the given
+     *            column.
+     */
+    public SortColumnCommand(ILayer layer, int columnPosition, boolean accumulate, SortDirectionEnum sortDirection) {
+        super(layer, columnPosition);
+        this.accumulate = accumulate;
+        this.sortDirection = sortDirection;
+    }
+
+    /**
+     * Constructor that is used to clone a command.
+     * 
+     * @param command
+     *            The command instance that should be cloned.
+     */
     protected SortColumnCommand(SortColumnCommand command) {
         super(command);
         this.accumulate = command.accumulate;
+        this.sortDirection = command.sortDirection;
     }
 
+    /**
+     *
+     * @return <code>true</code> if the sorting should be accumulated to an
+     *         already applied sorting, <code>false</code> if the sorting should
+     *         replace an existing sorting.
+     */
     public boolean isAccumulate() {
         return this.accumulate;
+    }
+
+    /**
+     *
+     * @return The sort direction that should be used for sorting. Can be
+     *         <code>null</code> which means that the sort direction will be
+     *         used that is calculated by the ISortModel for the given column.
+     */
+    public SortDirectionEnum getSortDirection() {
+        return this.sortDirection;
     }
 
     @Override
--- org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/sort/command/SortCommandHandler.java
+++ org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/sort/command/SortCommandHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Original authors and others.
+ * Copyright (c) 2012, 2014 Original authors and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Original authors and others - initial API and implementation
+ *     Dirk Fauth <dirk.fauth@googlemail.com> - Bug 454909
  ******************************************************************************/
 package org.eclipse.nebula.widgets.nattable.sort.command;
 
@@ -18,16 +19,19 @@ import org.eclipse.nebula.widgets.nattable.sort.event.SortColumnEvent;
 import org.eclipse.swt.custom.BusyIndicator;
 
 /**
- * Handle sort commands
+ * Command handler that handles {@link SortColumnCommand}s. Applies sorting via
+ * the {@link ISortModel} corresponding to the information that are send via the
+ * {@link SortColumnCommand}.
+ *
+ * @see SortHeaderLayer
+ * @see SortColumnCommand
  */
-public class SortCommandHandler<T> extends
-        AbstractLayerCommandHandler<SortColumnCommand> {
+public class SortCommandHandler<T> extends AbstractLayerCommandHandler<SortColumnCommand> {
 
     private final ISortModel sortModel;
     private final SortHeaderLayer<T> sortHeaderLayer;
 
-    public SortCommandHandler(ISortModel sortModel,
-            SortHeaderLayer<T> sortHeaderLayer) {
+    public SortCommandHandler(ISortModel sortModel, SortHeaderLayer<T> sortHeaderLayer) {
         this.sortModel = sortModel;
         this.sortHeaderLayer = sortHeaderLayer;
     }
@@ -35,24 +39,21 @@ public class SortCommandHandler<T> extends
     @Override
     public boolean doCommand(final SortColumnCommand command) {
 
-        final int columnIndex = command.getLayer().getColumnIndexByPosition(
-                command.getColumnPosition());
-        final SortDirectionEnum newSortDirection = this.sortModel.getSortDirection(
-                columnIndex).getNextSortDirection();
+        final int columnIndex = command.getLayer().getColumnIndexByPosition(command.getColumnPosition());
+        final SortDirectionEnum newSortDirection =
+                (command.getSortDirection()) != null ? command.getSortDirection() : this.sortModel.getSortDirection(columnIndex).getNextSortDirection();
 
         // Fire command - with busy indicator
         Runnable sortRunner = new Runnable() {
             @Override
             public void run() {
-                SortCommandHandler.this.sortModel.sort(columnIndex, newSortDirection,
-                        command.isAccumulate());
+                SortCommandHandler.this.sortModel.sort(columnIndex, newSortDirection, command.isAccumulate());
             }
         };
         BusyIndicator.showWhile(null, sortRunner);
 
         // Fire event
-        SortColumnEvent sortEvent = new SortColumnEvent(this.sortHeaderLayer,
-                command.getColumnPosition());
+        SortColumnEvent sortEvent = new SortColumnEvent(this.sortHeaderLayer, command.getColumnPosition());
         this.sortHeaderLayer.fireLayerEvent(sortEvent);
 
         return true;
