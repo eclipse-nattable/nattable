e2ef6b429433c4daf9934bb381d183659023659a
Dirk Fauth <dirk.fauth@googlemail.com>
2015-02-17 10:05:24 +0100

    Bug 459991 - Added null check for EditCellCommand members
    Change-Id: Idf7474fa2233353e55f2dc22aa79857ba7592f89
    Signed-off-by: Dirk Fauth <dirk.fauth@googlemail.com>

--- org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/edit/command/EditCellCommandHandler.java
+++ org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/edit/command/EditCellCommandHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2013 Original authors and others.
+ * Copyright (c) 2012, 2013, 2015 Original authors and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     Original authors and others - initial API and implementation
+ *     Dirk Fauth <dirk.fauth@googlemail.com> - Bug 459991
  ******************************************************************************/
 package org.eclipse.nebula.widgets.nattable.edit.command;
 
@@ -24,8 +25,7 @@ import org.eclipse.swt.widgets.Composite;
  * putting the cell into edit mode is allowed. If it is allowed it will call the
  * {@link EditController} for activation of the edit mode.
  */
-public class EditCellCommandHandler extends
-        AbstractLayerCommandHandler<EditCellCommand> {
+public class EditCellCommandHandler extends AbstractLayerCommandHandler<EditCellCommand> {
 
     @Override
     public Class<EditCellCommand> getCommandClass() {
@@ -37,21 +37,23 @@ public class EditCellCommandHandler extends
         ILayerCell cell = command.getCell();
         Composite parent = command.getParent();
         IConfigRegistry configRegistry = command.getConfigRegistry();
+        if (cell != null && configRegistry != null) {
 
-        // check if the cell is editable
-        IEditableRule rule = configRegistry.getConfigAttribute(
-                EditConfigAttributes.CELL_EDITABLE_RULE, DisplayMode.EDIT, cell
-                        .getConfigLabels().getLabels());
+            // check if the cell is editable
+            IEditableRule rule = configRegistry.getConfigAttribute(
+                    EditConfigAttributes.CELL_EDITABLE_RULE,
+                    DisplayMode.EDIT,
+                    cell.getConfigLabels().getLabels());
 
-        if (rule.isEditable(cell, configRegistry)) {
-            EditController.editCell(cell, parent, cell.getDataValue(),
-                    configRegistry);
+            if (rule.isEditable(cell, configRegistry)) {
+                EditController.editCell(
+                        cell, parent, cell.getDataValue(), configRegistry);
+            }
         }
 
         // as commands by default are intended to be consumed by the handler,
-        // always
-        // return true, whether the activation of the edit mode was successfull
-        // or not
+        // always return true, whether the activation of the edit mode was
+        // successful or not
         return true;
     }
 
