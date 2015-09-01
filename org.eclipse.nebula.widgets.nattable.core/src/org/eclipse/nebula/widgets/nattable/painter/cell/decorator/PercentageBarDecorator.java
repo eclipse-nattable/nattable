fe3c89d983b75496eb1ee03a2cd926682df3a2b0
Dirk Fauth <dirk.fauth@googlemail.com>
2015-09-01 09:17:53 +0200

    Bug 476273 - PercentageBarDecorator renders incorrectly within a grid
    Change-Id: I8cce98920420a074ea438f3fc11a3a0e3754a6e9
    Signed-off-by: Dirk Fauth <dirk.fauth@googlemail.com>

--- org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/painter/cell/decorator/PercentageBarDecorator.java
+++ org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/painter/cell/decorator/PercentageBarDecorator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Original authors and others.
+ * Copyright (c) 2012, 2015 Original authors and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -34,39 +34,46 @@ public class PercentageBarDecorator extends CellPainterWrapper {
     public static final ConfigAttribute<Color> PERCENTAGE_BAR_COMPLETE_REGION_END_COLOR = new ConfigAttribute<Color>();
     public static final ConfigAttribute<Color> PERCENTAGE_BAR_INCOMPLETE_REGION_COLOR = new ConfigAttribute<Color>();
 
-    private static final Color DEFAULT_COMPLETE_REGION_START_COLOR = GUIHelper
-            .getColor(new RGB(187, 216, 254));
-    private static final Color DEFAULT_COMPLETE_REGION_END_COLOR = GUIHelper
-            .getColor(new RGB(255, 255, 255));
+    private static final Color DEFAULT_COMPLETE_REGION_START_COLOR = GUIHelper.getColor(new RGB(187, 216, 254));
+    private static final Color DEFAULT_COMPLETE_REGION_END_COLOR = GUIHelper.getColor(new RGB(255, 255, 255));
 
     public PercentageBarDecorator(ICellPainter interiorPainter) {
         super(interiorPainter);
     }
 
     @Override
-    public void paintCell(ILayerCell cell, GC gc, Rectangle rectangle,
-            IConfigRegistry configRegistry) {
+    public void paintCell(ILayerCell cell, GC gc, Rectangle rectangle, IConfigRegistry configRegistry) {
         Pattern originalBackgroundPattern = gc.getBackgroundPattern();
 
-        double factor = Math.min(1.0,
-                ((Double) cell.getDataValue()).doubleValue());
+        double factor = Math.min(1.0, ((Double) cell.getDataValue()).doubleValue());
         factor = Math.max(0.0, factor);
 
-        Rectangle bar = new Rectangle(rectangle.x, rectangle.y,
-                (int) (rectangle.width * factor), rectangle.height);
-        Rectangle bounds = cell.getBounds();
+        Rectangle bar = new Rectangle(
+                rectangle.x,
+                rectangle.y,
+                (int) (rectangle.width * factor),
+                rectangle.height);
 
-        Color color1 = CellStyleUtil.getCellStyle(cell, configRegistry)
+        Color color1 = CellStyleUtil
+                .getCellStyle(cell, configRegistry)
                 .getAttributeValue(PERCENTAGE_BAR_COMPLETE_REGION_START_COLOR);
-        Color color2 = CellStyleUtil.getCellStyle(cell, configRegistry)
+        Color color2 = CellStyleUtil
+                .getCellStyle(cell, configRegistry)
                 .getAttributeValue(PERCENTAGE_BAR_COMPLETE_REGION_END_COLOR);
-        if (color1 == null)
+        if (color1 == null) {
             color1 = DEFAULT_COMPLETE_REGION_START_COLOR;
-        if (color2 == null)
+        }
+        if (color2 == null) {
             color2 = DEFAULT_COMPLETE_REGION_END_COLOR;
+        }
 
-        Pattern pattern = new Pattern(Display.getCurrent(), bounds.x, bounds.y,
-                bounds.x + bounds.width, bounds.y + bounds.height, color1,
+        Pattern pattern = new Pattern(
+                Display.getCurrent(),
+                rectangle.x,
+                rectangle.y,
+                rectangle.x + rectangle.width,
+                rectangle.y + rectangle.height,
+                color1,
                 color2);
         gc.setBackgroundPattern(pattern);
         gc.fillRectangle(bar);
@@ -74,9 +81,9 @@ public class PercentageBarDecorator extends CellPainterWrapper {
         gc.setBackgroundPattern(originalBackgroundPattern);
         pattern.dispose();
 
-        Color incompleteRegionColor = CellStyleUtil.getCellStyle(cell,
-                configRegistry).getAttributeValue(
-                PERCENTAGE_BAR_INCOMPLETE_REGION_COLOR);
+        Color incompleteRegionColor = CellStyleUtil
+                .getCellStyle(cell, configRegistry)
+                .getAttributeValue(PERCENTAGE_BAR_INCOMPLETE_REGION_COLOR);
         if (incompleteRegionColor != null) {
             Region incompleteRegion = new Region();
 
