4d1e8b4949fc95dafd23b6ee41245638c8e57552
Dirk Fauth <dirk.fauth@googlemail.com>
2015-09-01 16:58:27 +0200

    Bug 476348 - Add IDisplayConverter to support simple wildcard support in FilterRow
    Change-Id: Ie2cde2435d60458e758ba0230138b8c09ab1feb4
    Signed-off-by: Dirk Fauth <dirk.fauth@googlemail.com>

---
+++ org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/filterrow/FilterRowRegularExpressionConverter.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Dirk Fauth and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Dirk Fauth <dirk.fauth@googlemail.com> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.nebula.widgets.nattable.filterrow;
+
+import org.eclipse.nebula.widgets.nattable.data.convert.DisplayConverter;
+import org.eclipse.nebula.widgets.nattable.data.convert.IDisplayConverter;
+
+/**
+ * {@link IDisplayConverter} that is intended to be used for filter row editors
+ * that are configured for regular expression evaluation. It will add simplified
+ * usage of wildcards for end users by replacing * to the regular expression
+ * (.*) and ? to the regular expression (.?).
+ * <p>
+ * <b><u>Note:</u></b><br>
+ * As the characters * and ? are replaced to match the corresponding regular
+ * expression using this {@link IDisplayConverter} will suppress the ability to
+ * search for those characters completely. If it should be supported to search
+ * for those wildcards, implement a custom converter that supports to mask the
+ * icons or stick with the default regular expression syntax.
+ * </p>
+ *
+ * @since 1.4
+ */
+public class FilterRowRegularExpressionConverter extends DisplayConverter {
+
+    @Override
+    public Object displayToCanonicalValue(Object displayValue) {
+        if (displayValue != null) {
+            displayValue = displayValue.toString().replaceAll("\\*", "(.\\*)"); //$NON-NLS-1$ //$NON-NLS-2$
+            displayValue = displayValue.toString().replaceAll("\\?", "(.\\?)"); //$NON-NLS-1$ //$NON-NLS-2$
+        }
+        return displayValue;
+    }
+
+    @Override
+    public Object canonicalToDisplayValue(Object canonicalValue) {
+        if (canonicalValue != null) {
+            canonicalValue = canonicalValue.toString().replaceAll("\\(\\.\\*\\)", "\\*"); //$NON-NLS-1$ //$NON-NLS-2$
+            canonicalValue = canonicalValue.toString().replaceAll("\\(\\.\\?\\)", "\\?"); //$NON-NLS-1$ //$NON-NLS-2$
+        }
+        return canonicalValue;
+    }
+}
---
+++ org.eclipse.nebula.widgets.nattable.extension.glazedlists.test/src/org/eclipse/nebula/widgets/nattable/extension/glazedlists/filterrow/FilterRowRegularExpressionConverterTest.java
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Dirk Fauth and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Dirk Fauth <dirk.fauth@googlemail.com> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.nebula.widgets.nattable.extension.glazedlists.filterrow;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.nebula.widgets.nattable.filterrow.FilterRowRegularExpressionConverter;
+import org.junit.Test;
+
+public class FilterRowRegularExpressionConverterTest {
+
+    @Test
+    public void shouldConvertAsteriskFromDisplayToCanonical() {
+        FilterRowRegularExpressionConverter converter = new FilterRowRegularExpressionConverter();
+        assertEquals("(.*)", converter.displayToCanonicalValue("*"));
+        assertEquals("(.*)abc", converter.displayToCanonicalValue("*abc"));
+        assertEquals("abc(.*)", converter.displayToCanonicalValue("abc*"));
+        assertEquals("ab(.*)g", converter.displayToCanonicalValue("ab*g"));
+        assertEquals("(.*)abc(.*)", converter.displayToCanonicalValue("*abc*"));
+        assertEquals("(.*)ab(.*)cd(.*)", converter.displayToCanonicalValue("*ab*cd*"));
+    }
+
+    @Test
+    public void shouldConvertQuestionMarkFromDisplayToCanonical() {
+        FilterRowRegularExpressionConverter converter = new FilterRowRegularExpressionConverter();
+        assertEquals("(.?)", converter.displayToCanonicalValue("?"));
+        assertEquals("(.?)abc", converter.displayToCanonicalValue("?abc"));
+        assertEquals("abc(.?)", converter.displayToCanonicalValue("abc?"));
+        assertEquals("ab(.?)g", converter.displayToCanonicalValue("ab?g"));
+        assertEquals("(.?)abc(.?)", converter.displayToCanonicalValue("?abc?"));
+        assertEquals("(.?)ab(.?)cd(.?)", converter.displayToCanonicalValue("?ab?cd?"));
+    }
+
+    @Test
+    public void shouldConvertMixedFromDisplayToCanonical() {
+        FilterRowRegularExpressionConverter converter = new FilterRowRegularExpressionConverter();
+        assertEquals("(.*)abc(.?)", converter.displayToCanonicalValue("*abc?"));
+        assertEquals("(.?)abc(.*)", converter.displayToCanonicalValue("?abc*"));
+    }
+
+    @Test
+    public void shouldConvertAsteriskFromCanonicalToDisplay() {
+        FilterRowRegularExpressionConverter converter = new FilterRowRegularExpressionConverter();
+        assertEquals("*", converter.canonicalToDisplayValue("(.*)"));
+        assertEquals("*abc", converter.canonicalToDisplayValue("(.*)abc"));
+        assertEquals("abc*", converter.canonicalToDisplayValue("abc(.*)"));
+        assertEquals("ab*g", converter.canonicalToDisplayValue("ab(.*)g"));
+        assertEquals("*abc*", converter.canonicalToDisplayValue("(.*)abc(.*)"));
+        assertEquals("*ab*cd*", converter.canonicalToDisplayValue("(.*)ab(.*)cd(.*)"));
+    }
+
+    @Test
+    public void shouldConvertQuestionMarkFromCanonicalToDisplay() {
+        FilterRowRegularExpressionConverter converter = new FilterRowRegularExpressionConverter();
+        assertEquals("?", converter.canonicalToDisplayValue("(.?)"));
+        assertEquals("?abc", converter.canonicalToDisplayValue("(.?)abc"));
+        assertEquals("abc?", converter.canonicalToDisplayValue("abc(.?)"));
+        assertEquals("ab?g", converter.canonicalToDisplayValue("ab(.?)g"));
+        assertEquals("?abc?", converter.canonicalToDisplayValue("(.?)abc(.?)"));
+        assertEquals("?ab?cd?", converter.canonicalToDisplayValue("(.?)ab(.?)cd(.?)"));
+    }
+
+    @Test
+    public void shouldConvertMixedFromCanonicalToDisplay() {
+        FilterRowRegularExpressionConverter converter = new FilterRowRegularExpressionConverter();
+        assertEquals("*abc?", converter.canonicalToDisplayValue("(.*)abc(.?)"));
+        assertEquals("?abc*", converter.canonicalToDisplayValue("(.?)abc(.*)"));
+    }
+}
