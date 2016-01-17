b1e79071cc830a8cc1f2460250e768ad2b2e2931
Dirk Fauth <dirk.fauth@googlemail.com>
2016-01-17 20:50:33 +0100

    Bug 485921 - Export Excel raises NPE
    Change-Id: Ia19db987bdcffdeecacac91a1eaf8c65245a45db
    Signed-off-by: Dirk Fauth <dirk.fauth@googlemail.com>

--- org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/export/excel/DefaultExportFormatter.java
+++ org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/export/excel/DefaultExportFormatter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2014 Original authors and others.
+ * Copyright (c) 2012, 2015 Original authors and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,17 +7,20 @@
  *
  * Contributors:
  *     Original authors and others - initial API and implementation
- *     Dirk Fauth <dirk.fauth@googlemail.com> - Bug 448115, 449361
+ *     Dirk Fauth <dirk.fauth@googlemail.com> - Bug 448115, 449361, 485921
  ******************************************************************************/
 package org.eclipse.nebula.widgets.nattable.export.excel;
 
 import org.eclipse.nebula.widgets.nattable.config.CellConfigAttributes;
 import org.eclipse.nebula.widgets.nattable.config.IConfigRegistry;
+import org.eclipse.nebula.widgets.nattable.data.convert.DefaultDisplayConverter;
 import org.eclipse.nebula.widgets.nattable.data.convert.IDisplayConverter;
 import org.eclipse.nebula.widgets.nattable.export.IExportFormatter;
 import org.eclipse.nebula.widgets.nattable.layer.cell.ILayerCell;
 
 public class DefaultExportFormatter implements IExportFormatter {
+
+    private final IDisplayConverter fallbackConverter = new DefaultDisplayConverter();
 
     @Override
     public Object formatForExport(ILayerCell cell, IConfigRegistry configRegistry) {
@@ -26,6 +29,10 @@ public class DefaultExportFormatter implements IExportFormatter {
                 CellConfigAttributes.DISPLAY_CONVERTER,
                 cell.getDisplayMode(),
                 cell.getConfigLabels().getLabels());
+
+        if (displayConverter == null) {
+            displayConverter = this.fallbackConverter;
+        }
         return displayConverter.canonicalToDisplayValue(cell, configRegistry, dataValue);
     }
 
