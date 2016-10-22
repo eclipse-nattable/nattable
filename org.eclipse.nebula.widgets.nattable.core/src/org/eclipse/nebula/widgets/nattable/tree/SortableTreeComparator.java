1d5dbde64317f7dc9c7be58bbc19545549297a2b
Dirk Fauth <dirk.fauth@googlemail.com>
2016-10-22 21:34:24 +0200

    Bug 506394 - Fix SortableTreeComparator for same root element values
    Change-Id: I6252f8603c976d666f5212de8d392946a9f7e4d7
    Signed-off-by: Dirk Fauth <dirk.fauth@googlemail.com>

--- org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/tree/SortableTreeComparator.java
+++ org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/tree/SortableTreeComparator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2015 Original authors and others.
+ * Copyright (c) 2012, 2016 Original authors and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -58,6 +58,7 @@ public class SortableTreeComparator<T> implements Comparator<T> {
                         }
                     }
                 }
+                comparators.add(this.treeComparator);
                 return new ComparatorChain<T>(comparators).compare(o1, o2);
             } else {
                 return treeComparatorResult;
