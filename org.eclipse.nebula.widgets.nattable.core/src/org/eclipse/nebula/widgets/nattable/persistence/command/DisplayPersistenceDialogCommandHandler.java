e1e92d797430758bd334172a2bd1528eae479132
Dirk Fauth <dirk.fauth@googlemail.com>
2017-03-08 22:46:11 +0100

    Bug 513308 - DisplayPersistenceDialogCommandHandler needs to be registered after configure()
    Change-Id: I5ca59c1c69109883ffdc507d616f0bc23f210de9
    Signed-off-by: Dirk Fauth <dirk.fauth@googlemail.com>

--- org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/persistence/command/DisplayPersistenceDialogCommandHandler.java
+++ org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/persistence/command/DisplayPersistenceDialogCommandHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Dirk Fauth and others.
+ * Copyright (c) 2012, 2017 Dirk Fauth and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,17 +22,14 @@ import org.eclipse.nebula.widgets.nattable.persistence.gui.PersistenceDialog;
  * Command handler implementation for handling
  * {@link DisplayPersistenceDialogCommand}s. It is used to open the
  * corresponding dialog for save/load operations regarding the NatTable state.
- * Will also serve as some kind of storage for the Properties instance holding
- * the states.
- *
- * @author Dirk Fauth
- *
+ * Will also serve as some kind of storage for the {@link Properties} instance
+ * holding the states.
  */
-public class DisplayPersistenceDialogCommandHandler extends
-        AbstractLayerCommandHandler<DisplayPersistenceDialogCommand> {
+public class DisplayPersistenceDialogCommandHandler extends AbstractLayerCommandHandler<DisplayPersistenceDialogCommand> {
 
     /**
-     * The Properties instance that should be used for saving and loading.
+     * The {@link Properties} instance that should be used for saving and
+     * loading.
      */
     private Properties properties;
 
@@ -45,25 +42,35 @@ public class DisplayPersistenceDialogCommandHandler extends
      * propagated to the newly created dialog that is created. This way the
      * listeners only need to be registered once from a users point of view,
      * while this handler will deal the correct registering.
+     * </p>
      */
     private List<IStateChangedListener> stateChangeListeners = new ArrayList<IStateChangedListener>();
 
     /**
-     * Create a new DisplayPersistenceDialogCommandHandler. Using this
-     * constructor the Properties instance used for save and load operations
-     * will be created. It can be accessed via getProperties() for further
-     * usage.
+     * Create a new {@link DisplayPersistenceDialogCommandHandler}. Using this
+     * constructor the {@link Properties} instance used for save and load
+     * operations will be created. It can be accessed via
+     * {@link #getProperties()} for further usage.
      */
     public DisplayPersistenceDialogCommandHandler() {
         this(new Properties(), null);
     }
 
     /**
-     * Create a new DisplayPersistenceDialogCommandHandler. Using this
-     * constructor the Properties instance used for save and load operations
-     * will be created. It can be accessed via getProperties() for further
-     * usage. The current state of the given NatTable instance will be used to
-     * store a default configuration.
+     * Create a new {@link DisplayPersistenceDialogCommandHandler}. Using this
+     * constructor the {@link Properties} instance used for save and load
+     * operations will be created. It can be accessed via
+     * {@link #getProperties()} for further usage. The current state of the
+     * given NatTable instance will be used to store a default configuration.
+     * <p>
+     * <b>IMPORTANT:</b><br>
+     * To store a default configuration in the view configuration management
+     * based on a given NatTable instance, you need to create and register the
+     * {@link DisplayPersistenceDialogCommandHandler} <b><i>AFTER</i></b>
+     * calling {@link NatTable#configure()}. Otherwise some state information
+     * might be missing and it could cause some unexpected behavior, like broken
+     * sorting for example.
+     * </p>
      *
      * @param natTable
      *            The NatTable instance for which this handler is registered. If
@@ -76,33 +83,41 @@ public class DisplayPersistenceDialogCommandHandler extends
     }
 
     /**
-     * Create a new DisplayPersistenceDialogCommandHandler using the specified
-     * Properties instance.
+     * Create a new {@link DisplayPersistenceDialogCommandHandler} using the
+     * specified {@link Properties} instance.
      *
      * @param properties
-     *            The Properties instance that should be used for saving and
-     *            loading.
+     *            The {@link Properties} instance that should be used for saving
+     *            and loading.
      */
     public DisplayPersistenceDialogCommandHandler(Properties properties) {
         this(properties, null);
     }
 
     /**
-     * Create a new DisplayPersistenceDialogCommandHandler using the specified
-     * Properties instance. The current state of the given NatTable instance
-     * will be used to store a default configuration.
+     * Create a new {@link DisplayPersistenceDialogCommandHandler} using the
+     * specified {@link Properties} instance. The current state of the given
+     * NatTable instance will be used to store a default configuration.
+     * <p>
+     * <b>IMPORTANT:</b><br>
+     * To store a default configuration in the view configuration management
+     * based on a given NatTable instance, you need to create and register the
+     * {@link DisplayPersistenceDialogCommandHandler} <b><i>AFTER</i></b>
+     * calling {@link NatTable#configure()}. Otherwise some state information
+     * might be missing and it could cause some unexpected behavior, like broken
+     * sorting for example.
+     * </p>
      *
      * @param properties
-     *            The Properties instance that should be used for saving and
-     *            loading.
+     *            The {@link Properties} instance that should be used for saving
+     *            and loading.
      * @param natTable
      *            The NatTable instance for which this handler is registered. If
      *            it is not <code>null</code>, the current state of that
      *            NatTable will be stored as default configuration. This default
      *            configuration can't be modified anymore in the opened dialog.
      */
-    public DisplayPersistenceDialogCommandHandler(Properties properties,
-            NatTable natTable) {
+    public DisplayPersistenceDialogCommandHandler(Properties properties, NatTable natTable) {
         if (properties == null) {
             throw new IllegalArgumentException("properties can not be null!"); //$NON-NLS-1$
         }
@@ -110,22 +125,16 @@ public class DisplayPersistenceDialogCommandHandler extends
 
         if (natTable != null) {
             natTable.saveState("", this.properties); //$NON-NLS-1$
-            this.properties.setProperty(
-                    PersistenceDialog.ACTIVE_VIEW_CONFIGURATION_KEY, ""); //$NON-NLS-1$
+            this.properties.setProperty(PersistenceDialog.ACTIVE_VIEW_CONFIGURATION_KEY, ""); //$NON-NLS-1$
         }
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.eclipse.nebula.widgets.nattable.command.AbstractLayerCommandHandler
-     * #doCommand(org.eclipse.nebula.widgets.nattable.command.ILayerCommand)
-     */
     @Override
     protected boolean doCommand(DisplayPersistenceDialogCommand command) {
-        PersistenceDialog dialog = new PersistenceDialog(command.getNatTable()
-                .getShell(), command.getNatTable(), this.properties);
+        PersistenceDialog dialog = new PersistenceDialog(
+                command.getNatTable().getShell(),
+                command.getNatTable(),
+                this.properties);
         // register the listeners
         dialog.addAllStateChangeListener(this.stateChangeListeners);
         // open the dialog
@@ -175,12 +184,6 @@ public class DisplayPersistenceDialogCommandHandler extends
         this.stateChangeListeners.remove(listener);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see org.eclipse.nebula.widgets.nattable.command.ILayerCommandHandler#
-     * getCommandClass()
-     */
     @Override
     public Class<DisplayPersistenceDialogCommand> getCommandClass() {
         return DisplayPersistenceDialogCommand.class;
