d94599e84529783cb1180ef1ba6e74354eecbba7
Dirk Fauth <dirk.fauth@googlemail.com>
2019-01-24 21:26:06 +0100

    Bug 538321 - [Freeze] Scrollbar should not vanish if window size is changed
    Fixed a regression that empty tables always show a disabled scrollbar
    
    Change-Id: Ic319f7014be399420ff80bdea54835e1d75ccbe4
    Signed-off-by: Dirk Fauth <dirk.fauth@googlemail.com>

--- org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/viewport/ScrollBarHandlerTemplate.java
+++ org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/viewport/ScrollBarHandlerTemplate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2018 Original authors and others.
+ * Copyright (c) 2012, 2019 Original authors and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -137,7 +137,7 @@ public abstract class ScrollBarHandlerTemplate implements Listener {
         } else {
             this.scroller.setThumb(max);
             this.scroller.setEnabled(false);
-            this.scroller.setVisible(viewportWindowSpan == 0);
+            this.scroller.setVisible(viewportWindowSpan == 0 && max > 0);
         }
     }
 
