6bb9a53bf2cbe0116c62cc285ad32fa6c7786100
Dirk Fauth <dirk.fauth@googlemail.com>
2026-01-07 10:03:55 +0100

    Impl #182 - Extend background painter to blend with selection style
    Signed-off-by: Dirk Fauth <dirk.fauth@googlemail.com>

--- org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/painter/cell/BackgroundPainter.java
+++ org.eclipse.nebula.widgets.nattable.core/src/org/eclipse/nebula/widgets/nattable/painter/cell/BackgroundPainter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2020 Original authors and others.
+ * Copyright (c) 2012, 2026 Original authors and others.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -12,13 +12,18 @@
  ******************************************************************************/
 package org.eclipse.nebula.widgets.nattable.painter.cell;
 
+import org.eclipse.nebula.widgets.nattable.config.CellConfigAttributes;
 import org.eclipse.nebula.widgets.nattable.config.ConfigRegistry;
 import org.eclipse.nebula.widgets.nattable.config.IConfigRegistry;
 import org.eclipse.nebula.widgets.nattable.layer.cell.ILayerCell;
 import org.eclipse.nebula.widgets.nattable.style.CellStyleAttributes;
+import org.eclipse.nebula.widgets.nattable.style.CellStyleProxy;
 import org.eclipse.nebula.widgets.nattable.style.CellStyleUtil;
+import org.eclipse.nebula.widgets.nattable.style.DisplayMode;
+import org.eclipse.nebula.widgets.nattable.util.GUIHelper;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.graphics.Rectangle;
 
 /**
@@ -55,9 +60,64 @@ public class BackgroundPainter extends CellPainterWrapper {
         super.paintCell(cell, gc, bounds, configRegistry);
     }
 
+    /**
+     * Get the background color for the given cell.
+     *
+     * @param cell
+     *            The {@link ILayerCell} to get the background color for.
+     * @param configRegistry
+     *            The {@link ConfigRegistry} needed to retrieve the color.
+     * @return The background color.
+     */
     protected Color getBackgroundColour(ILayerCell cell, IConfigRegistry configRegistry) {
+        Boolean blend = configRegistry.getConfigAttribute(
+                CellConfigAttributes.BLEND_SELECTION_BACKGROUND,
+                DisplayMode.NORMAL,
+                cell.getConfigLabels());
+        // if blending is enabled and the cell is selected, return blended color
+        if (blend != null && blend
+                && (cell.getDisplayMode() == DisplayMode.SELECT || cell.getDisplayMode() == DisplayMode.SELECT_HOVER)) {
+            return getBlendedBackgroundColour(cell, configRegistry);
+        }
+
         return CellStyleUtil
                 .getCellStyle(cell, configRegistry)
                 .getAttributeValue(CellStyleAttributes.BACKGROUND_COLOR);
+    }
+
+    /**
+     * Get the blended background color for the given cell by blending the
+     * normal and the select background colors.
+     *
+     * @param cell
+     *            The {@link ILayerCell} to get the blended background color
+     *            for.
+     * @param configRegistry
+     *            The {@link ConfigRegistry} needed to retrieve the colors.
+     * @return The blended background color.
+     *
+     * @since 2.7
+     */
+    protected Color getBlendedBackgroundColour(ILayerCell cell, IConfigRegistry configRegistry) {
+        // get background color for DisplayMode.NORMAL
+        Color normalBackground =
+                new CellStyleProxy(
+                        configRegistry,
+                        DisplayMode.NORMAL,
+                        cell.getConfigLabels())
+                                .getAttributeValue(CellStyleAttributes.BACKGROUND_COLOR);
+
+        // get background color for DisplayMode.SELECT or DisplayMode.SELECT_HOVER
+        Color selectBackground =
+                new CellStyleProxy(
+                        configRegistry,
+                        cell.getDisplayMode(),
+                        cell.getConfigLabels())
+                                .getAttributeValue(CellStyleAttributes.BACKGROUND_COLOR);
+
+        // blend both colors
+        RGB background = normalBackground.getRGB();
+        background = GUIHelper.blend(background, selectBackground.getRGB());
+        return GUIHelper.getColor(background);
     }
 }
